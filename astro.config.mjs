db5ec71cbb83425132afb3dd9bf3d6149dde86d5
Zell Liew <zellwk@gmail.com>
2022-05-24 19:21:27 +0800

    Add Site

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import svelte from '@astrojs/svelte'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://buildanddeploy.today',
   integrations: [svelte()],
   vite: {
     server: {
